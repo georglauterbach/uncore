a18d849497aaaa763719d27f2ae8793c1b4b6a9a
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2023-11-14 15:36:27 +0100

    use proper Cargo aliases

--- misc/scripts/uncore_commands.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-alias  help="cargo run    -- help"
-alias   run="cargo run -q -- run"
-alias debug="cargo run -q -- run --debug"
-alias utest="cargo run -q -- u-test"
-alias itest="cargo run -q -- i-test"
-alias check="cargo run -q -- check"
